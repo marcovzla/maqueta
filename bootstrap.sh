160cc9914aec2e9428a53fd9546686a02814f5f2
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-08-19 23:44:33 -0700

    added support for jMonkeyEngine

---
+++ bootstrap.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+
+# downloads directory
+DL='downloads'
+
+# local maven repository
+REPO='maven-repo'
+
+
+
+# create directories if needed and get absolute paths
+[ -d $DL ] || mkdir $DL
+cd $DL
+DL=`pwd`
+cd - &> /dev/null
+
+[ -d $REPO ] || mkdir $REPO
+cd $REPO
+REPO=`pwd`
+cd - &> /dev/null
+
+
+
+# download file from given url
+function get-file {
+    FILE="$DL/${1##*/}"
+    curl $1 -o $FILE
+}
+
+# install jar in local repo
+function mvn-deploy {
+    mvn deploy:deploy-file -Dfile=$1 \
+                           -DgroupId=local \
+                           -DartifactId=$(basename $1 .jar) \
+                           -Dversion=$2 \
+                           -Dpackaging=jar \
+                           -Durl=file:$REPO
+}
+
+
+
+# jMonkeyEngine 3.0
+get-file 'http://www.jmonkeyengine.com/nightly/jME3_2012-08-02.zip'
+unzip $FILE -d "$DL/jme3"
+for jar in $DL/jme3/lib/*.jar; do
+    mvn-deploy $jar 3.0
+done
--- project.clj
+++ project.clj
@@ -3,4 +3,32 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 ;; jMonkeyEngine 3.0
+                 [local/eventbus "3.0"]
+                 [local/jbullet "3.0"]
+                 [local/jinput "3.0"]
+                 [local/jME3-blender "3.0"]
+                 [local/jME3-core "3.0"]
+                 [local/jME3-desktop "3.0"]
+                 [local/jME3-effects "3.0"]
+                 [local/jME3-jbullet "3.0"]
+                 [local/jME3-jogg "3.0"]
+                 [local/jME3-lwjgl "3.0"]
+                 [local/jME3-lwjgl-natives "3.0"]
+                 [local/jME3-networking "3.0"]
+                 [local/jME3-niftygui "3.0"]
+                 [local/jME3-plugins "3.0"]
+                 [local/jME3-terrain "3.0"]
+                 [local/jME3-testdata "3.0"]
+                 [local/j-ogg-oggd "3.0"]
+                 [local/j-ogg-vorbisd "3.0"]
+                 [local/lwjgl "3.0"]
+                 [local/nifty "3.0"]
+                 [local/nifty-default-controls "3.0"]
+                 [local/nifty-examples "3.0"]
+                 [local/nifty-style-black "3.0"]
+                 [local/stack-alloc "3.0"]
+                 [local/vecmath "3.0"]
+                 [local/xmlpull-xpp3 "3.0"]]
+  :repositories {"project" "file:maven-repo"})
