39eec2ce312e15b735b9875a6886834bf30159ed
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-08-21 15:55:14 -0700

    jme3 tutorial 4
    http://jmonkeyengine.org/wiki/doku.php/jme3:beginner:hello_main_event_loop

--- src/maqueta/core.clj
+++ src/maqueta/core.clj
@@ -1,59 +1,30 @@
 (ns maqueta.core
   (:use maqueta.assets)
-  (:import (com.jme3.app SimpleApplication)
-           (com.jme3.system AppSettings)
-           (com.jme3.font BitmapText)
-           (com.jme3.light DirectionalLight)
-           (com.jme3.math Vector3f)
-           (com.jme3.scene Geometry)
-           (com.jme3.scene.shape Box))
+  (:import com.jme3.app.SimpleApplication
+           com.jme3.system.AppSettings
+           com.jme3.math.ColorRGBA
+           com.jme3.math.Vector3f
+           com.jme3.scene.Geometry
+           com.jme3.scene.shape.Box)
   (:gen-class))
 
 (def ^:dynamic *app-settings* (doto (AppSettings. true)
                                 (.setFullscreen false)
                                 (.setTitle "maqueta")))
 
+(def player (let [box (Box. Vector3f/ZERO 1 1 1)
+                  geo (Geometry. "blue cube" box)
+                  mat (load-material "Common/MatDefs/Misc/Unshaded.j3md")]
+              (.setColor mat "Color" ColorRGBA/Blue)
+              (.setMaterial geo mat)
+              geo))
+
 (def app (proxy [SimpleApplication] []
            (simpleInitApp []
-             (let [teapot (load-model "Models/Teapot/Teapot.obj")
-                   mat-default (load-material "Common/MatDefs/Misc/ShowNormals.j3md")
-                   wall (Geometry. "Box" (Box. Vector3f/ZERO 2.5 2.5 1.0))
-                   mat-brick (load-material "Common/MatDefs/Misc/Unshaded.j3md")
-                   gui-font (load-font "Interface/Fonts/Default.fnt")
-                   hello-text (BitmapText. gui-font false)
-                   ninja (load-model "Models/Ninja/Ninja.mesh.xml")
-                   sun (DirectionalLight.)]
+             (.attachChild (.getRootNode this) player))
 
-               (.setMaterial teapot mat-default)
-
-               (.setTexture mat-brick "ColorMap"
-                            (load-texture "Textures/Terrain/BrickWall/BrickWall.jpg"))
-
-               (doto wall
-                 (.setMaterial mat-brick)
-                 (.setLocalTranslation 2.0 -2.5 0.0))
-
-               (doto hello-text
-                 (.setSize (.getRenderedSize (.getCharSet gui-font)))
-                 (.setText "Hello World")
-                 (.setLocalTranslation 300 (.getLineHeight hello-text) 0))
-
-               (doto (.getGuiNode this)
-                 .detachAllChildren
-                 (.attachChild hello-text))
-
-               (doto ninja
-                 (.scale 0.05 0.05 0.05)
-                 (.rotate 0.0 -3.0 0.0)
-                 (.setLocalTranslation 0.0 -5.0 -2.0))
-
-               (.setDirection sun (Vector3f. -0.1 -0.7 -1.0))
-
-               (doto (.getRootNode this)
-                 (.attachChild teapot)
-                 (.attachChild wall)
-                 (.attachChild ninja)
-                 (.addLight sun))))))
+           (simpleUpdate [tpf]
+             (.rotate player 0 (* 2 tpf) 0))))
 
 (defn -main [& args]
   (doto app
