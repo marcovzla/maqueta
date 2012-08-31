1d5bfb9d10eaea8b991369bdb0335a304ec47eb2
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-08-30 19:56:52 -0700

    loaded oto golem

--- src/maqueta/core.clj
+++ src/maqueta/core.clj
@@ -2,20 +2,26 @@
   (:use (maqueta application assets))
   (:import com.jme3.math.ColorRGBA
            com.jme3.math.Vector3f
-           com.jme3.scene.Geometry
-           com.jme3.scene.shape.Box)
+           com.jme3.scene.Node
+           com.jme3.light.DirectionalLight)
   (:gen-class))
 
-(def player (let [box (Box. Vector3f/ZERO 1 1 1)
-                  geo (Geometry. "blue cube" box)
-                  mat (load-material "Common/MatDefs/Misc/Unshaded.j3md")]
-              (.setColor mat "Color" ColorRGBA/Blue)
-              (.setMaterial geo mat)
-              geo))
+(def light (DirectionalLight.))
+
+(def player (load-model "Models/Oto/Oto.mesh.xml"))
+
+(defn setup-fn
+  [app]
+  (let [root-node (.getRootNode app)
+        viewport (.getViewPort app)]
+    (.setBackgroundColor viewport ColorRGBA/LightGray)
+    (.setDirection light (.normalizeLocal (Vector3f. -0.1 -1 -1)))
+    (.addLight root-node light)
+    (.setLocalScale player (Vector3f. 0.5 0.5 0.5))))
 
 (defn -main [& args]
   (.start (make-app player
-                    no-op
+                    setup-fn
                     no-op
                     {:key-h (fn [n p t] (.rotate player 0 1 0))}
                     {:key-j (fn [n v t] (.rotate player 0 v 0))})))
