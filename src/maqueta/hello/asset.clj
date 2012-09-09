5a13c1b7a12049312049d130f328019cd6886dbb
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-09-09 15:24:28 -0700

    fixed indentation

--- src/maqueta/hello/asset.clj
+++ src/maqueta/hello/asset.clj
@@ -9,44 +9,44 @@
 (defn setup-fn
   [app]
   (let [teapot (load-model "Models/Teapot/Teapot.obj")
-                   mat-default (load-material "Common/MatDefs/Misc/ShowNormals.j3md")
-                   wall (Geometry. "Box" (Box. Vector3f/ZERO 2.5 2.5 1.0))
-                   mat-brick (load-material "Common/MatDefs/Misc/Unshaded.j3md")
-                   gui-font (load-font "Interface/Fonts/Default.fnt")
-                   hello-text (BitmapText. gui-font false)
-                   ninja (load-model "Models/Ninja/Ninja.mesh.xml")
-                   sun (DirectionalLight.)]
+        mat-default (load-material "Common/MatDefs/Misc/ShowNormals.j3md")
+        wall (Geometry. "Box" (Box. Vector3f/ZERO 2.5 2.5 1.0))
+        mat-brick (load-material "Common/MatDefs/Misc/Unshaded.j3md")
+        gui-font (load-font "Interface/Fonts/Default.fnt")
+        hello-text (BitmapText. gui-font false)
+        ninja (load-model "Models/Ninja/Ninja.mesh.xml")
+        sun (DirectionalLight.)]
 
-               (.setMaterial teapot mat-default)
+    (.setMaterial teapot mat-default)
 
-               (.setTexture mat-brick "ColorMap"
-                            (load-texture "Textures/Terrain/BrickWall/BrickWall.jpg"))
+    (.setTexture mat-brick "ColorMap"
+                 (load-texture "Textures/Terrain/BrickWall/BrickWall.jpg"))
 
-               (doto wall
-                 (.setMaterial mat-brick)
-                 (.setLocalTranslation 2.0 -2.5 0.0))
+    (doto wall
+      (.setMaterial mat-brick)
+      (.setLocalTranslation 2.0 -2.5 0.0))
 
-               (doto hello-text
-                 (.setSize (.getRenderedSize (.getCharSet gui-font)))
-                 (.setText "Hello World")
-                 (.setLocalTranslation 300 (.getLineHeight hello-text) 0))
+    (doto hello-text
+      (.setSize (.getRenderedSize (.getCharSet gui-font)))
+      (.setText "Hello World")
+      (.setLocalTranslation 300 (.getLineHeight hello-text) 0))
 
-               (doto (.getGuiNode app)
-                 .detachAllChildren
-                 (.attachChild hello-text))
+    (doto (.getGuiNode app)
+      .detachAllChildren
+      (.attachChild hello-text))
 
-               (doto ninja
-                 (.scale 0.05 0.05 0.05)
-                 (.rotate 0.0 -3.0 0.0)
-                 (.setLocalTranslation 0.0 -5.0 -2.0))
+    (doto ninja
+      (.scale 0.05 0.05 0.05)
+      (.rotate 0.0 -3.0 0.0)
+      (.setLocalTranslation 0.0 -5.0 -2.0))
 
-               (.setDirection sun (Vector3f. -0.1 -0.7 -1.0))
+    (.setDirection sun (Vector3f. -0.1 -0.7 -1.0))
 
-               (doto (.getRootNode app)
-                 (.attachChild teapot)
-                 (.attachChild wall)
-                 (.attachChild ninja)
-                 (.addLight sun))))
+    (doto (.getRootNode app)
+      (.attachChild teapot)
+      (.attachChild wall)
+      (.attachChild ninja)
+      (.addLight sun))))
 
 (defn -main
   []
