13df8ebca0069bb4646bfacebc681e51c5f0e4f0
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-09-09 02:44:05 -0700

    added tutorial 2

---
+++ src/maqueta/tutorials/node.clj
@@ -0,0 +1,28 @@
+(ns maqueta.tutorials.node
+  (:use (maqueta application assets))
+  (:import com.jme3.scene.shape.Box
+           (com.jme3.scene Geometry Node)
+           (com.jme3.math ColorRGBA Vector3f)))
+
+(defn setup-fn
+  [app]
+  (let [box1 (Box. (Vector3f. 1 -1 1) 1 1 1)
+        blue (Geometry. "Box" box1)
+        mat1 (load-material "Common/MatDefs/Misc/Unshaded.j3md")
+        box2 (Box. (Vector3f. 1 3 1) 1 1 1)
+        red (Geometry. "Box" box2)
+        mat2 (load-material "Common/MatDefs/Misc/Unshaded.j3md")
+        pivot (Node. "pivot")]
+    (.setColor mat1 "Color" ColorRGBA/Blue)
+    (.setMaterial blue mat1)
+    (.setColor mat2 "Color" ColorRGBA/Red)
+    (.setMaterial red mat2)
+    (.attachChild (.getRootNode app)
+                  (doto pivot
+                    (.attachChild blue)
+                    (.attachChild red)
+                    (.rotate 0.4 0.4 0)))))
+
+(defn -main
+  []
+  (.start (make-app :setup-fn setup-fn)))
