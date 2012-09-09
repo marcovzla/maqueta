04c9edfd618a5912a82c9605ddbc7bb2c1888b10
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-09-09 02:06:41 -0700

    start saving tutorials

---
+++ src/maqueta/tutorials/simple_application.clj
@@ -0,0 +1,17 @@
+(ns maqueta.tutorials.simple-application
+  (:use (maqueta application assets))
+  (:import com.jme3.scene.Geometry
+           com.jme3.scene.shape.Box
+           (com.jme3.math ColorRGBA Vector3f))
+  (:gen-class))
+
+(def geom (let [b (Box. Vector3f/ZERO 1 1 1)
+                geom (Geometry. "Box" b)
+                mat (load-material "Common/MatDefs/Misc/Unshaded.j3md")]
+            (.setColor mat "Color" ColorRGBA/Blue)
+            (.setMaterial geom mat)
+            geom))
+
+(defn -main
+  []
+  (.start (make-app :root-node geom)))
