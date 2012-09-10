c37134126c73a8be069058cfef6fa001a132d784
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2012-09-10 11:53:51 -0700

    edited animation.clj

--- src/maqueta/hello/animation.clj
+++ src/maqueta/hello/animation.clj
@@ -3,11 +3,9 @@
 
 (ns maqueta.hello.animation
   (:use (maqueta application assets))
-  (:import com.jme3.math.ColorRGBA
-           com.jme3.math.Vector3f
-           com.jme3.scene.Node
-           (com.jme3.animation AnimControl LoopMode)
-           com.jme3.light.DirectionalLight))
+  (:import com.jme3.light.DirectionalLight
+           (com.jme3.math ColorRGBA Vector3f)
+           (com.jme3.animation AnimControl LoopMode)))
 
 (def light (DirectionalLight.))
 
@@ -24,8 +22,6 @@
       (.setAnim "Walk" 0.5)
       (.setLoopMode LoopMode/Loop))))
 
-(def on-action {:key-space walk})
-
 (defn cycle-done
   [app control channel]
   (doto channel
@@ -33,22 +29,21 @@
     (.setLoopMode LoopMode/DontLoop)
     (.setSpeed 1)))
 
-(def on-anim-cycle-done {"Walk" cycle-done})
-
 (defn setup-fn
   [app]
   (let [root-node (.getRootNode app)
         viewport (.getViewPort app)]
     (.setBackgroundColor viewport ColorRGBA/LightGray)
     (.setDirection light (.normalizeLocal (Vector3f. -0.1 -1 -1)))
-    (.addLight root-node light)
     (.setLocalScale player (Vector3f. 0.5 0.5 0.5))
     (.addListener control app)
-    (.setAnim channel "stand")))
+    (.setAnim channel "stand")
+    (doto root-node
+      (.attachChild player)
+      (.addLight light))))
 
 (defn -main
   [& args]
-  (.start (make-app :root-node player
-                    :setup-fn setup-fn
-                    :on-anim-cycle-done on-anim-cycle-done
-                    :on-action on-action)))
+  (.start (make-app :setup-fn setup-fn
+                    :on-action {:key-space walk}
+                    :on-anim-cycle-done {"Walk" cycle-done})))
